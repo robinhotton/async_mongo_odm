304d8838c4b4f709d3d298aa6f7040c1266858b0
Robin HOTTON <robin.hotton@hotmail.com>
2024-10-03 15:59:23 +0200

    perf(classe): refacto ClasseResponse

--- app/services/classe_service.py
+++ app/services/classe_service.py
@@ -4,36 +4,36 @@ from ..models import Classe
 from ..schemas import ClasseResponse, ClasseCreate, ClasseUpdate
 
 
+def _create_response(classe: Classe) -> ClasseResponse:
+    reponse: ClasseResponse = ClasseResponse(
+        _id=str(classe.id),
+        nom=classe.nom,
+        prof=str(classe.prof.to_dict()["id"]) if classe.prof else None)
+    return reponse
+
+
 async def get_classes(skip, limit) -> List[ClasseResponse]:
     classes: List[Classe] = await Classe.find_all().skip(skip).limit(limit).to_list()
-    response: List[ClasseResponse] = [ClasseResponse(
-        _id=str(classe.id), 
-        nom=classe.nom, 
-        prof=str(classe.prof.to_dict()["id"]) if classe.prof else None) 
-        for classe in classes]
-    return response
+    return [_create_response(classe) for classe in classes]
 
 
 async def get_classe(classe_id: ObjectId) -> Optional[ClasseResponse]:
     classe: Classe = await Classe.get(classe_id)
-    response: ClasseResponse = ClasseResponse(_id=str(classe.id), nom=classe.nom, prof=str(classe.prof.to_dict()["id"]) if classe.prof else None)
-    return response
+    return _create_response(classe) if classe else None
 
 
 async def create_classe(classe_data: ClasseCreate) -> ClasseResponse:
     classe: Classe = Classe(**classe_data.model_dump())
     await classe.insert()
-    response: ClasseResponse = ClasseResponse(_id=str(classe.id), nom=classe.nom, prof=str(classe.prof.to_dict()["id"]) if classe.prof else None)
-    return response
+    return _create_response(classe) if classe else None
 
 
 async def update_classe(classe_id: ObjectId, classe_data: ClasseUpdate) -> Optional[Classe]:
     classe: Classe = await Classe.get(classe_id)
     if classe:
         update_data: ClasseUpdate = classe_data.model_dump(exclude_unset=True)
-        result = await classe.set(update_data)
-        response: ClasseResponse = ClasseResponse(_id=str(classe.id), nom=classe.nom, prof=str(classe.prof.to_dict()["id"]) if classe.prof else None)
-        return response
+        await classe.set(update_data)
+        return _create_response(classe) if classe else None
     return None
 
 
